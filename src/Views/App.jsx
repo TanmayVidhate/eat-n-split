e2ddff9d266274fcb9ac9e2b3219e26e1e6b0668
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-08 18:40:59 +0530

    conditional rendering

--- src/Views/App.jsx
+++ src/Views/App.jsx
@@ -5,14 +5,23 @@ import FormAddFriend from '../Components/FormAddFriend'
 import Button from '../Components/Button'
 import FormBillSplit from "../Components/FormBillSplit"
 function App() {
+  const [isOpenFormAddFriend , setIsOpenFormAddFriend] = useState(false)
+
+  const handleFormOpen = () => {
+    setIsOpenFormAddFriend((isOpenFormAddFriend) =>!isOpenFormAddFriend )
+    
+  }
 
   return (
     <>
       <div className='app'>
         <div className='sidebar'>
           <FriendsList />
-          <FormAddFriend />
-          <Button>Add friend</Button>
+          {
+            isOpenFormAddFriend ? <FormAddFriend /> : <p>{""}</p>
+          }
+          
+          <Button onClick = {() => handleFormOpen()} >Add friend</Button>
         </div>
       <FormBillSplit/>
       </div>
