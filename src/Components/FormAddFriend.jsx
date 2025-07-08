74f17439ef23ff0de124c22549373fa0e2b0a608
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-08 19:16:56 +0530

    add controlling input

--- src/Components/FormAddFriend.jsx
+++ src/Components/FormAddFriend.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import  { useState } from 'react'
 import Button from './Button'
 
 function FormAddFriend() {
+  const [name,setName] = useState("")
+  const [img,setImg] = useState("")
+
+  
   return (
     <>
         <form className='form-add-friend'>
-            <label>👩🏻‍🤝‍👩🏻Friend Name</label>
-            <input type='text'/>
+            <label>👩🏻‍🤝‍👩🏻 Friend Name</label>
+            <input type='text' value={name} onChange={(e) => {setName(e.target.value)}}/>
 
             <label>📸 Img URL</label>
-            <input type='text'/>
+            <input type='text' value={img} onChange={(e) => {setImg(e.target.value)}}/>
 
             <Button>Add</Button>
         </form>
