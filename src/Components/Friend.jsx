c34bd09a42206a072c6b3d9cd03d67a3518a0074
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-08 16:51:27 +0530

    data render in Friend Component

--- src/Components/Friend.jsx
+++ src/Components/Friend.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
-function Friend({friend :{id,name,image,balance}}) {
-  return (
-    <>
-        <li>
-            <p>{name}</p>
-        </li>
-    </>
-  )
+function Friend({ friend: { id, name, image, balance } }) {
+    return (
+        <>
+            <li>
+                <img src={image} alt={name} />
+                <h3>{name}</h3>
+                {
+                    balance < 0 ? <p className='red' >You own {name} {Math.abs(balance)}</p> : balance > 0 ? <p className='green'>{name} own {balance}</p> : balance === 0 ? <p>You and {name} are even</p> : null
+                }
+                <button className='button'>Select</button>
+            </li>
+        </>
+    )
 }
 
 export default Friend
--- src/Views/App.jsx
+++ src/Views/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
 import FriendsList from '../Components/FriendsList'
 function App() {
-  
+
   return (
     <>
       <div className='app'>
         <div className='sidebar'>
-           <FriendsList/>
+          <FriendsList />
         </div>
       </div>
     </>
